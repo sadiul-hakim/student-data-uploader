f5b6edaf38d6f5dc5d863b524bfd34ddd65aafb9
sadiul-hakim <sadiulhakim@gmail.com>
2024-05-16 13:14:12 +0600

    Revert "Update"
    This reverts commit 7820303a7de924cfff97c803e8251b8c458520be.

--- src/main/java/com/hakim/datauploder/DatauploderApplication.java
+++ src/main/java/com/hakim/datauploder/DatauploderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataUploaderApplication {
+public class DatauploderApplication {
 	public static void main(String[] args) {
 
-		SpringApplication.run(DataUploaderApplication.class, args);
+		SpringApplication.run(DatauploderApplication.class, args);
 	}
 
 }
